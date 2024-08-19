fbd5213d3047c956ab9279d360afc1a6ace888eb
Tosin-Mayowa <toss800@gmail.com>
2024-08-18 22:33:40 -0700

    connect to local mongodb

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const environment=process.env.NODE_ENV || 'development';
 require('dotenv').config({path:`config.env.${environment}`});
+const { default: mongoose } = require('mongoose');
 const app=require('./index')
 const {PORT}=process.env;
+mongoose.connect(process.env.LOCAL_CONN_STR).then(()=>{
+    console.log('connection to mongodb sucessful');
+}).catch(err=>{
+    console.log('fail to connect to mongodb',err);
+    
+})
 
 app.listen(PORT || 8000,()=>{
     console.log('server running on port:'+ PORT);
