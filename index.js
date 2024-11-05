46e8d3590b87c6bbbd43d70265a65b11991b0559
Tosin-Mayowa <toss800@gmail.com>
2024-11-05 02:39:58 -0800

    cors

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app=express();
 
 
 const allowedOrigins = ['http://localhost:5173', 'http://localhost:5174',
-    'http://localhost:5175','https://e-learning-express-server.vercel.app'];
+    'http://localhost:5175','https://e-learning-client-sqkb.vercel.app/'];
 
 const corsOptions = {
     origin: (origin, callback) => {
